2da7283d21c3fddc10cc85502da9cf86968c58f7
Ethan Yang <1195767760@qq.com>
2020-02-03 21:26:57 +0000

    api fix

--- app/api/modSchedule/index.php
+++ app/api/modSchedule/index.php
@@ -19,7 +19,7 @@ if (isset($_POST['weekday']) && trim($_POST['weekday']) != '') {
         exit(json_encode($response));
     }
 } else {
-    $response->message = 'Wrong Execution';
+    $response->message = 'Wrong Execution' . '-' . $_POST['weekday'];
     $response->Code = '22222';
     $response->data = array();
     exit(json_encode($response));
