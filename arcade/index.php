d38da460939db355e23354d36abc4fa6e0b81807
Ethan Yang <1195767760@qq.com>
2020-03-07 05:08:44 +0000

    test

--- arcade/index.php
+++ arcade/index.php
@@ -3,14 +3,261 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>923 | Game Room</title>
+    <title>Arcade | Mafia</title>
     <link rel="shortcut icon" href="/favicon.png">
-    <link rel="stylesheet" href="/css/global.css">
+    <!-- <link rel="stylesheet" href="/css/global.css"> -->
+    <style>
+        html,
+        body {
+            width: 100%;
+            padding: 0;
+            margin: 0;
+        }
+
+        html {
+            /* background-color: rgb(186, 128, 252); */
+            background: linear-gradient(180deg, #6e8efb, #a777e3);
+        }
+
+        p.head {
+            position: fixed;
+            z-index: 1;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            /* width: 400px; */
+            /* height: 500px; */
+            padding: 0;
+            margin: 0;
+            /* background-color: grey; */
+            /* display: block; */
+        }
+
+        p.head {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 16rem;
+            font-weight: bold;
+            color: rgba(61, 61, 61, 0.151);
+        }
+
+        div.content {
+            position: relative;
+            z-index: 10;
+            width: 100%;
+            /* width: 500px; */
+            /* height: 900px; */
+            /* max-width: 800px; */
+            /* position: absolute; */
+            margin-top: calc(100vh - 80px);
+        }
+
+        div.contentBackground {
+            position: absolute;
+            top: 80px;
+            width: inherit;
+            background-color: #f8f8f8;
+            box-shadow: -20px 0px 50px rgba(80, 80, 80, 0.568);
+            bottom: 0;
+        }
+
+        div.contentContainer {
+            position: relative;
+            padding: 80px 50px;
+            max-width: 1200px;
+            margin: 0 auto;
+            background-color: white;
+            box-shadow: 0px 10px 20px rgba(1, 1, 1, 0.1);
+            text-align: center;
+            border-radius: 20px;
+            z-index: 9;
+        }
+
+        h1.title {
+            position: fixed;
+            z-index: 2;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+            font-size: 100px;
+            font-family: Arial, Helvetica, sans-serif;
+            color: white;
+        }
+
+        #role {
+            color: rgb(255, 184, 53);
+            font-size: 140px;
+        }
+
+        .item {
+            position: relative;
+            margin: 0;
+            margin-top: 50px;
+            /* padding: 40px 20px; */
+            height: 50px;
+            background-color: thistle;
+        }
+
+        .items {
+            padding: 0;
+        }
+
+        .items li {
+            list-style-type: none;
+            display: list-item;
+            padding-bottom: 80px;
+            text-align: left;
+
+        }
+
+        .content li h2 {
+            padding: 0;
+            margin: 0;
+            font-size: 3em;
+        }
+
+        .bg {
+            overflow: hidden;
+            border-radius: 20px;
+            height: 400px;
+            width: 100%;
+            background: url('/arcade/topdown/topdown.png') no-repeat center;
+            background-size: cover;
+            box-shadow: 0px 20px 30px rgba(1, 1, 1, 0.26);
+        }
+
+        div.caption {
+            /* margin-top: 400px; */
+            transform: translateY(-100%);
+            width: 100%;
+            text-align: center;
+            background-color: rgba(151, 151, 151, 0.301);
+            color: white;
+            z-index: 11;
+        }
+
+        div.mask {
+            transition: 0.5s;
+            height: inherit;
+            width: inherit;
+            z-index: 13;
+            color: transparent;
+            background: linear-gradient(90deg, rgba(255, 111, 0, 0), rgba(255, 145, 0, 0));
+        }
+
+        div.mask p {
+            margin: 0;
+            display: block;
+            /* position: absolute; */
+            /* right: 0; */
+            padding: 30px 40px;
+            font-size: 1.5rem;
+            /* margin-inline-end: 40px; */
+        }
+
+        div.mask:hover {
+            transition: 1s;
+            background: linear-gradient(90deg, rgba(255, 111, 0, 1), rgba(255, 145, 0, 1));
+            color: white;
+        }
+
+        div.footer {
+            height: 250px;
+            position: relative;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+            z-index: 5;
+        }
+
+        div.footer a {
+            font-size: 23px;
+            text-decoration: none;
+            color: rgba(255, 145, 0, 1);
+        }
+
+        #name {
+            font-size: 26px;
+            margin-bottom: 20px;
+        }
+
+        div.naviBar {
+            width: 100%;
+            position: fixed;
+            top: 0;
+            left: 0;
+            overflow: hidden;
+            /* background-color: rgba(0, 0, 0, 0); */
+            background: linear-gradient(rgba(231, 231, 231, 0.164), rgba(0, 0, 0, 0));
+            display: flex;
+            align-items: center;
+        }
+
+        ul.nav {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+            /* overflow: auto; */
+            width: inherit;
+        }
+
+        ul.nav li {
+            float: left;
+        }
+
+        .nav li.end {
+            float: right;
+        }
+
+        div.naviBar a {
+            display: block;
+            font-weight: bold;
+            color: white;
+            font-size: 20px;
+            background-color: transparent;
+            text-align: center;
+            padding: 20px 30px;
+            text-decoration: none;
+        }
+
+        div.naviBar a:hover,
+        a:active {
+            background-color: rgba(39, 39, 39, 0.075);
+        }
+
+        li.end a {
+            display: block;
+            font-weight: lighter;
+            color: white;
+            font-size: 20px;
+            background-color: transparent;
+            text-align: center;
+            padding: 20px 30px;
+            text-decoration: none;
+        }
+
+        #logo {
+            display: inline-block;
+            padding: 0;
+            margin: 0;
+            width: 50px;
+            height: 50px;
+        }
+
+        .nav li div {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100px;
+            height: 63px;
+        }
+    </style>
 </head>
 
 <body>
     <div class="naviBar">
-        <ul>
+        <ul class="nav">
             <li>
                 <div><img id="logo" src="/logo.svg"></div>
             </li>
@@ -34,8 +281,95 @@
             ?>
         </ul>
     </div>
-    <div class="welcome">
-        <h1 class="title"><b>WELCOME<br>TO<br><i id="role">Arcade</i><br>923</b></h1>
+
+
+    <p class="head"><i>
+            ARCADE
+        </i></p>
+    <h1 class="title"><b>WELCOME<br>TO<br><i id="role">Arcade</i><br>923</b></h1>
+    <div class="content">
+        <div class="contentBackground"></div>
+        <!-- <div class="contentContainer">
+            <h2 style="font-size: 5em; padding: 0; margin: 0;">Hello World!</h2>
+        </div> -->
+        <div class="contentContainer" style="margin-top: 50px;">
+            <ul class="items">
+                <li>
+                    <div class="bg" onmouseover="" onmouseout="" id="i">
+                        <div class="mask">
+                            <p>
+                                <b>THIS IS THE DESCRIPTION OF THE GAME</b>
+                                <br>
+                                <br>
+                                This text field should be provided by the game maker to describ his or her game. The
+                                description here should include the style of the game, for example,
+                                how many people can be involved, whether it needs good quality of internet connection,
+                                the average/typical time per round. It should be in detail but
+                                concise.
+                            </p>
+                        </div>
+                        <a href="/arcade/topdown/" style="display: block; text-decoration: none;">
+                            <div class="caption">
+                                <h2 style="font-weight: normal;">Topdown</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="bg" onmouseover="" onmouseout="" id="i">
+                        <div class="mask">
+                            <p>
+                                <b>THIS IS THE DESCRIPTION OF THE GAME</b>
+                                <br>
+                                <br>
+                                This text field should be provided by the game maker to describ his or her game. The
+                                description here should include the style of the game, for example,
+                                how many people can be involved, whether it needs good quality of internet connection,
+                                the average/typical time per round. It should be in detail but
+                                concise.
+                            </p>
+                        </div>
+                        <a href="/arcade/topdown/" style="display: block; text-decoration: none;">
+                            <div class="caption">
+                                <h2 style="font-weight: normal;">Topdown</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+
+                <li style="padding-bottom: 0;">
+                    <div class="bg" onmouseover="" onmouseout="" id="i">
+                        <div class="mask">
+                            <p>
+                                <b>THIS IS THE DESCRIPTION OF THE GAME</b>
+                                <br>
+                                <br>
+                                This text field should be provided by the game maker to describ his or her game. The
+                                description here should include the style of the game, for example,
+                                how many people can be involved, whether it needs good quality of internet connection,
+                                the average/typical time per round. It should be in detail but
+                                concise.
+                            </p>
+                        </div>
+                        <a href="/arcade/topdown/" style="display: block; text-decoration: none;">
+                            <div class="caption">
+                                <h2 style="font-weight: normal;">Topdown</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="footer">
+            <div class="declaration" style="color: #aaa;">
+                <div id="name">
+                    <h2>Room923</h2>
+                </div>
+                <div id="author">Created by <a href="https://github.com/EthanGeekFan/room923/">Room923</a> | <a href="https://github.com/EthanGeekFan/">Yifan Yang</a></div>
+                <div style="margin-top: 15px;">All Rights Reserved</div>
+            </div>
+        </div>
     </div>
 </body>
 
---
+++ arcade/jndex.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+    <meta charset="UTF-8">
+    <title>923 | Game Room</title>
+    <link rel="shortcut icon" href="/favicon.png">
+    <link rel="stylesheet" href="/css/global.css">
+</head>
+
+<body>
+    <div class="naviBar">
+        <ul>
+            <li>
+                <div><img id="logo" src="/logo.svg"></div>
+            </li>
+            <li><a href="/">Room 923</a></li>
+            <li><a href="/arcade/">Arcade</a></li>
+            <li><a href="/gallery/">Gallery</a></li>
+            <li><a href="/blog/">Blogs</a></li>
+            <?php
+            $login = false;
+
+            session_start();
+
+            if (isset($_SESSION['login']) && $_SESSION['login'] == 1) {
+                $username = $_SESSION['username'];
+                echo '<li class="end"><a href="/login/logout.php">Log Out</a></li>';
+                echo '<li class="end"><a href="">Hi, ' . $username . '! </a></li>';
+            } else {
+                echo '<li class="end"><a href="/login/">Login</a></li>';
+                echo '<li class="end"><a href="/login/?signup=true">Sign up</a></li>';
+            }
+            ?>
+        </ul>
+    </div>
+    <div class="welcome">
+        <h1 class="title"><b>WELCOME<br>TO<br><i id="role">Arcade</i><br>923</b></h1>
+    </div>
+</body>
+
+</html>
